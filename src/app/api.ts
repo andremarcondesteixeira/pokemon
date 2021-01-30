6d029f6e26785b3847fd11ef467d56cc422a3270
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-01-30 20:43:17 -0300

    a small refactoring in api.ts

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
-import { Pokemon } from '../features/pokemon-list/Pokemon';
 
 type PokemonListResponse = {
   count: number;
   next: string;
   previous: string;
-  results: Pokemon[];
+  results: {
+    name: string;
+    url: string;
+  }[];
 }
 
 const get = async <T>(url: string) => (await axios.get<T>(`https://pokeapi.co/api/v2/${url}`)).data;
