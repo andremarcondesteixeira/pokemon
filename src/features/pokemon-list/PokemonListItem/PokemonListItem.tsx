0e419447232836eeaba3896cae5aa8dbf498c179
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-02-01 01:42:11 -0300

    creates PokemonListItem component

---
+++ src/features/pokemon-list/PokemonListItem/PokemonListItem.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { PokemonResponse } from '../../../app/api';
+
+export function PokemonListItem(props: { pokemon: PokemonResponse}) {
+  return <div>teste</div>;
+}
