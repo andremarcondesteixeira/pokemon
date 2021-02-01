e283167041b768c62320ad9b789d8626c45ff37b
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2021-01-31 22:41:00 -0300

    removes unneeded features

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import pokemonListReducer from '../features/pokemon-list/PokemonListSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     pokemonsList: pokemonListReducer
   },
 });
